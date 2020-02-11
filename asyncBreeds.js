b3887be53444f9636d58fad68bd7ccfa02e2c362
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-11 21:18:10 +0000

    created mocha tests

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -3,10 +3,16 @@ const fs = require('fs');
 const breedDetailsFromFile = function(breed, cb) {
 console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    if (!error) return cb(data);
+    if (!error) {
+      return cb(data);
+    } else {
+      return cb(undefined)
+    }
   });
 };
 
 let callBack = breed => console.log(breed);
 
 breedDetailsFromFile('bombay', callBack);
+
+module.exports = breedDetailsFromFile;
--- syncBreeds.js
+++ syncBreeds.js
@@ -1,7 +1,7 @@
-// const catBreeds = {
-//   'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
-//   'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
-// };
+const catBreeds = {
+ 'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+  'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
+};
 
 // synchronous function to fetch a cat breed
 const breedDetails = function(breed) {
@@ -12,3 +12,5 @@ const breedDetails = function(breed) {
 // get the return value right away from the function
 const bombay = breedDetails('Bombay');
 console.log(bombay); //=> prints out the description for that breed
+
+module.exports = breedDetails;
